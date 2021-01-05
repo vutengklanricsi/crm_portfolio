107eb7daaec761db7a865ce92a6d002d53b62e3a
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-01-05 12:14:04 +0100

    adding routes to components

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <h1>
+           Navbar Page 
+        </h1>
+    )
+}
---
+++ src/components/Post.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Post() {
+    return (
+        <h1>
+           Post Page 
+        </h1>
+    )
+}
