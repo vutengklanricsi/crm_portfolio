9d8f7dd6c72ad4fd5506e5030ba983709f164436
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-01-05 14:24:35 +0100

    adding navbar to page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import About from "./components/About";
 import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
 import Project from "./components/Project";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <h1>
-           Navbar Page 
-        </h1>
-    )
+  return (
+    <header>
+      <div>
+        <nav>
+          <NavLink to="/" exact>
+            Richard
+          </NavLink>
+          <NavLink to="post">Blog Posts</NavLink>
+          <NavLink to="project">Projects</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 }
