8c399fa83fa31fb15f94aa3a50e3fed7465eb6fd
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-01-06 13:56:00 +0100

    adding post to post page

--- src/components/Post.js
+++ src/components/Post.js
@@ -25,7 +25,7 @@ export default function Post() {
   }, []);
 
   return (
-    <main className="bg-green-100 min-h-screen p-12">
+    <main className="bg-blue-100 min-h-screen p-12">
       <section className="container mx-auto">
         <h1 className="text-5xl flex justify-center cursive">
           My Blog Posts Page!
@@ -39,7 +39,7 @@ export default function Post() {
               <article>
                 <Link to={"/post/" + post.slug.current} key={post.slug.current}>
                   <span
-                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-blue-400"
                     key={index}
                   >
                     <img
@@ -48,7 +48,7 @@ export default function Post() {
                       className="w-full h-full rounded-r object-cover absolute"
                     />
                     <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                      <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                      <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-blue-700 text-blue-100 bg-opacity-75 rounded">
                         {post.title}
                       </h3>
                     </span>
