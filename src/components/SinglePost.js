eadd7e7925213a63098e775189e16270972629df
vutengklanricsi <richard.vuongtanquang@gmail.com>
2021-01-07 13:08:08 +0100

    fixing bug to display singlepost

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
-import sanityClient from "../client";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import sanityClient from "../client.js";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
 const builder = imageUrlBuilder(sanityClient);
-
 function urlFor(source) {
   return builder.image(source);
 }
@@ -17,32 +16,30 @@ export default function SinglePost() {
   useEffect(() => {
     sanityClient
       .fetch(
-        `
-			*[slug.current == "${slug}"]{
-				title,
-				_id,
-				slug,
-				mainImage{
-						asset->
-						url,
-						id
-				}
-			},
-			body,
-				"name": author->name,
-				"authorImage": author->image
-		`
+        `*[slug.current == "${slug}"]{
+        title,
+        _id,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+              url
+            }
+          },
+          body,
+          "name": author->name,
+          "authorImage": author->image
+        }`
       )
       .then((data) => {
         console.log(data);
-        setSinglePost(data);
+        setSinglePost(data[0]);
       })
-      .catch((error) => console.log(error));
+      .catch(console.error);
   }, [slug]);
 
-  if (!singlePost) {
-    return <div>Loading...</div>;
-  }
+  if (!singlePost) return <div>Loading...</div>;
+
   return (
     <main className="bg-gray-200 min-h-screen p-12">
       <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
